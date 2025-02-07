a062ec26a7250c2a5a0a16e745f497d31b345e07
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-07 20:25:02 +0000

    create droneserialzer using the SlugRelatedField

--- drones/serializers.py
+++ drones/serializers.py
@@ -21,3 +21,18 @@ class DroneCategorySerializer(serializers.HyperlinkedModelSerializer):
             'name',
             'drones'
         )
+
+class DroneSerializer(serializers.HyperlinkedModelSerializer):
+    drone_category = serializers.SlugRelatedField(queryset=DroneCategory.objects.all(),
+                                                  slug_field='name')
+
+    class Meta:
+        model = Drone
+        fields = (
+            'url',
+            'name',
+            'drone_category',
+            'manufacturing_date',
+            'has_it_competed',
+            'inserted_timestamp'
+        )
