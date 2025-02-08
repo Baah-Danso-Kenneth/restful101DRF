9670ac77a2d96832419db1c7ce3a5dde5f0cc397
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-08 14:00:40 +0000

    Add PilotSerializer with PilotComeptitionSerializer

--- drones/serializers.py
+++ drones/serializers.py
@@ -48,3 +48,41 @@ class CompetitionSerializer(serializers.HyperlinkedModelSerializer):
             'distance_achievement_date',
             'drone'
         )
+
+class PilotSerializer(serializers.HyperlinkedModelSerializer):
+    competitions = CompetitionSerializer(many=True, read_only=True)
+    gender = serializers.ChoiceField(
+        choices=Pilot.GENDER_CHOICES
+    )
+    gender_description = serializers.CharField(
+        source='get_gender_display',
+        read_only=True
+    )
+
+    class Meta:
+        model = Pilot
+        fields = (
+            'url',
+            'name',
+            'gender',
+            'gender_description',
+            'races_count',
+            'inserted_timestamp',
+            'competitions'
+        )
+
+class PilotCompetitionSerializer(serializers.ModelSerializer):
+    pilot = serializers.SlugRelatedField(queryset=Pilot.objects.all(),slug_field='name')
+    drone = serializers.SlugRelatedField(queryset=Drone.objects.all(), slug_field='name')
+
+    class Meta:
+        model = Competition
+        fields = (
+            'url',
+            'pk',
+            'distance_in_feet',
+            'distance_achievement_date',
+            'pilot',
+            'drone'
+        )
+    pass
